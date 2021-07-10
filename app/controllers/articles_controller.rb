ca5fc7fe91f8c5db50357f9139dc0c4fa822001e
Samer Alswairki <samer.alswairki@gmail.com>
2021-07-10 22:20:33 +0300

    adding validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
@@ -17,9 +17,14 @@ class ArticlesController < ApplicationController
     #we need below to white list what we are getting from the browser first: it is called strong parameter
     @article = Article.new(params.require(:article).permit(:title, :description))
     #render plain: @article.inspect
-    @article.save
-    #redirect_to article_path(@article)
-    # or a shorter code for the same output
-    redirect_to @article
+    if @article.save
+      #below flash message needs to be iterated in application.html.erb main file to be seen right after the creation of an article
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    #redirect_to @article
+    # or a shorter code for the same output as above
   end
 end
