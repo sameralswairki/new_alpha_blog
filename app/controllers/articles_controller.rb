24ab064724de91d0f1be3e495ceaded88ae21126
Samer Alswairki <samer.alswairki@gmail.com>
2021-07-10 14:15:48 +0300

    Creating new article from the browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,19 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]
+    #we need below to white list what we are getting from the browser first: it is called strong parameter
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    @article.save
+    #redirect_to article_path(@article)
+    # or a shorter code for the same output
+    redirect_to @article
+  end
 end
