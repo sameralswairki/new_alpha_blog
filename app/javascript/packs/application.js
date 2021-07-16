4100d7a173d635901dc79cf0fd17b7b7c48d5185
Samer Alswairki <samer.alswairki@gmail.com>
2021-07-16 22:40:04 +0300

    Build home page using bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "bootstrap"
+
+
+
