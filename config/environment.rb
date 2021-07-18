97e3caaf8d5065bebfbbbaeca892648bd4b72de5
Samer Alswairki <samer.alswairki@gmail.com>
2021-07-18 18:52:16 +0300

    styling flash and validation messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#to disable field of error created by rails while inspecting the error message
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
