141a3739c7057e430e39ea29a458b9a1ebeb7eeb
Samer Alswairki <samer.alswairki@gmail.com>
2021-07-24 17:15:42 +0300

    adding sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   #resources provided REST-ful routes to Rails resources
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
 
